92a5cd331a095898e80e389665172abafa28ceda
Dmitry Anisov <anis.dim@gmail.com>
2022-05-08 21:31:29 +0300

    add dark theme support

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,32 +4,46 @@ import { Link } from "gatsby";
 
 const FooterStyled = styled.footer`
   width: 100%;
-  background-color: var(--color-main);
-  bottom: 0;
+  border-top: 1px solid var(--color-background-gray);
+  position: relative;
 `;
-const Container = styled.div`
+const FooterInfoContainer = styled.div`
   display: flex;
+  flex-direction: column;
   max-width: 1100px;
+  align-items: left;
+  padding: 3rem 1rem;
+  margin: 0 auto;
+  gap: 2rem;
+`;
+
+const LinkContainer = styled.div`
+  display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: left;
-  padding: 1rem;
-  margin: 0 auto;
 `;
 
 const StyledLink = styled((props) => <Link {...props} />)`
-  color: var(--color-text-white);
+  color: ${props => props.theme};
   text-decoration: none;
 `;
+
 const Footer = () => {
   return (
     <FooterStyled>
-      <Container>
-        <StyledLink to="/">Link 1</StyledLink>
-        <StyledLink to="/">Link 2</StyledLink>
-        <StyledLink to="/">Link 3</StyledLink>
-      </Container>
+      <FooterInfoContainer>
+        <LinkContainer>
+          <h3>Links</h3>
+          <StyledLink to="/">Link 1</StyledLink>
+          <StyledLink to="/">Link 2</StyledLink>
+          <StyledLink to="/">Link 3</StyledLink>
+        </LinkContainer>
+        <div>
+          <p>© 2022 - present Dmitry Anisov.</p>
+          <p>All rights reserved.</p>
+        </div>
+      </FooterInfoContainer>
     </FooterStyled>
   )
-}  
+}
 export default Footer;
--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,19 @@
 import * as React from "react";
 import styled from "styled-components";
 
-const Container = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
+const Container = styled.main`
   margin: 0 auto;
   width: 100%;
   max-width: 1100px;
+`;
+
+const StyledH1 = styled.h1`
+  grid-column: 1 / -1;
+`;
+
+const StyledSection = styled.section`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
   padding-block: min(10vh, 10rem);
   padding-left: 1rem;
   padding-right: 1rem;
@@ -18,15 +25,13 @@ const Container = styled.div`
   }
 `;
 
-const StyledH1 = styled.h1`
-  grid-column: 1 / -1;
-`;
-
 const Main = ({children, title}) => {
   return (
       <Container>
-        <StyledH1>{title}</StyledH1>
-        {children}
+        <StyledSection>
+          <StyledH1>{title}</StyledH1>
+          {children}
+        </StyledSection>
       </Container>
   )
 }
