0d6deb19afb477a739888552acad4f61cea6f91e
Dmitry Anisov <anis.dim@gmail.com>
2022-06-16 14:10:44 +0300

    move og-image

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -68,7 +68,7 @@ const BlogPost = ({ data, location }) => {
       article={true}
       datePublished={data.mdx.frontmatter.datePublished}
       dateModified={data.mdx.frontmatter.dateModified}
-      image={`/posts/${data.mdx.slug}.png`}
+      image={`/og-images/${data.mdx.slug}.png`}
     >
       <Main>
         <ArticleHero
