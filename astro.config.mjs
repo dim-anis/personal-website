10439fb0ffd289ae4eae45e1deb3369da75778f5
dim-anis <anis.dim@gmail.com>
2024-03-15 19:57:26 +0700

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dmitryanisov.com",
   markdown: {
     shikiConfig: {
-      theme: "rose-pine-moon",
-    },
+      theme: "rose-pine-moon"
+    }
   },
-  integrations: [react(), mdx(), sitemap()],
+  integrations: [react(), mdx(), sitemap(), tailwind()]
 });
-
