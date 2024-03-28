b918c8032593faf57c783c2ed8fc5a2fe94e3ddc
dim-anis <anis.dim@gmail.com>
2024-03-28 19:06:18 +0700

    update site description

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,3 @@
 export const SITE_TITLE = "Dmitry Anisov";
-export const SITE_DESCRIPTION = "Join me on a learning adventure through software development, as I share insights on developer tools, productivity, and my journey with new programming languages.";
+export const SITE_DESCRIPTION =
+  "Sharing my insights on developer tools, productivity, learning new programming languages and more!";
